502ce903f3a3b3b43498441923108ec5c6f0e416
garyDav <gary.2810.dav@gmail.com>
2021-07-22 15:12:03 -0400

    first commit

---
+++ src/main/java/com/iofullstack/mgmt/config/CustomRevisionListener.java
@@ -0,0 +1,8 @@
+package com.iofullstack.mgmt.config;
+
+import com.iofullstack.mgmt.entities.audit.Revision;
+import org.hibernate.envers.RevisionListener;
+
+public class CustomRevisionListener implements RevisionListener {
+    public void newRevision(Object revisionEntity) { final Revision revision = (Revision) revisionEntity; }
+}
---
+++ src/test/java/com/iofullstack/mgmt/MgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iofullstack.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
