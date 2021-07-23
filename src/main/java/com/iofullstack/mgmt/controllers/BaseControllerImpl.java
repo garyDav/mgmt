c3825bbbc8633bca392fcd7528981c4623b1d024
garyDav <gary.2810.dav@gmail.com>
2021-07-22 23:58:22 -0400

    Probando la paginación con Postman

--- src/main/java/com/iofullstack/mgmt/controllers/BaseControllerImpl.java
+++ src/main/java/com/iofullstack/mgmt/controllers/BaseControllerImpl.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.Serializable;
-
 public abstract class BaseControllerImpl<E extends Base, S extends BaseServiceImpl<E, Long>> implements BaseController<E, Long> {
     @Autowired
     protected S servicio;
