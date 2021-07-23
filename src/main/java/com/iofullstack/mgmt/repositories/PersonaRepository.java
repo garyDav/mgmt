9dbcf0ec27e71999eed2d07e898d5e829336c0d0
garyDav <gary.2810.dav@gmail.com>
2021-07-22 22:18:52 -0400

    JPA Queries: @Query & Query Methods

--- src/main/java/com/iofullstack/mgmt/repositories/PersonaRepository.java
+++ src/main/java/com/iofullstack/mgmt/repositories/PersonaRepository.java
@@ -1,8 +1,24 @@
 package com.iofullstack.mgmt.repositories;
 
 import com.iofullstack.mgmt.entities.Persona;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PersonaRepository extends BaseRepository<Persona, Long> {
+    List<Persona> findByNombreContainingOrApellidoContaining(String nombre, String apellido);
+
+    // boolean existsByDni(int dni);
+    /*@Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE %?1%")
+    List<Persona> search(String filtro);*/
+    /*@Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE %:filtro% OR p.apellido LIKE %:filtro%")
+    List<Persona> search(@Param("filtro") String filtro);*/
+    @Query(
+            value = "SELECT * FROM persona as p WHERE p.nombre LIKE %:filtro% OR p.apellido LIKE %:filtro%",
+            nativeQuery = true
+    )
+    List<Persona> searchNativo(@Param("filtro") String filtro);
 }
